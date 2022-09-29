2eb6a4072659713547ce2e1ddd43b470f6a09777
luis <beholderluis@gmail.com>
2022-09-29 11:49:13 -0500

    segundo commit

--- Examen/app1/views.py
+++ Examen/app1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def suma(val):
+    return print(val)
+def post_list(request):
+    consulta = Post.objects.all()
+    cont = {'datos':consulta}
+    return render(request, 'app1/post_list.html', cont, function=suma(5))
