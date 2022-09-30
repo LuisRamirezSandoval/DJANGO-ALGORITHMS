7f19270c614d6a1addd81207505f92555ab425bd
luis <beholderluis@gmail.com>
2022-09-29 22:13:15 -0500

    tercer commit

--- Examen/app1/views.py
+++ Examen/app1/views.py
@@ -6,5 +6,11 @@ from .models import Post
 def post_list(request):
     consulta = Post.objects.all()
     cont = {'datos':consulta}
-    
+    datos = list(Post.objects.all())
+    suma=[]
+    for dato in datos:
+        val = dato.num1+dato.num3+dato.num4
+        suma.append(val)
+    cont['suma']=suma
+    # print(cont)
     return render(request, 'app1/post_list.html', cont)
