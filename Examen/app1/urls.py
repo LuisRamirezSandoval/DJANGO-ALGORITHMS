b5ef0a4307aa78ba3cfa6ceb65a4a44e4014dfcd
luis <beholderluis@gmail.com>
2022-11-06 18:34:35 -0600

    herencia templates 2

--- Examen/app1/urls.py
+++ Examen/app1/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('hola/', views.hola_mundo, name='hola_mundo')
+    path(r'', views.post_list, name='post_list'),
+    path('hola_mundo/', views.hola_mundo, name='hola_mundo'),
 ]
